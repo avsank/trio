265ba59f8a2defbc71ece6be04446082d009f5bf
Anna Shabalina <shabalina@gmail.com>
2021-05-10 16:38:45 -0400

    Anna's 2nd commit

--- src/test/java/HM_practice.java
+++ src/test/java/HM_practice.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class HM_practice {
     public static void main(String []args){
         System.out.println("Testing from terminal");
+        System.out.println("Second commit is required");
     }
 
 }
