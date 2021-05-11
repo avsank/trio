d91ce2fccda488bb70d3394de2c43cd250217287
Anna Shabalina <shabalina@gmail.com>
2021-05-10 20:21:05 -0400

    changes are added

--- src/test/java/HM_practice.java
+++ src/test/java/HM_practice.java
@@ -4,6 +4,12 @@ public class HM_practice {
     public static void main(String []args){
         System.out.println("Testing from terminal");
         System.out.println("Second commit is required");
+
+        String str = "Anna";
+        for(int i =0; i<str.length();  i++) {
+            System.out.println(str);
+        }
+
     }
 
 }
